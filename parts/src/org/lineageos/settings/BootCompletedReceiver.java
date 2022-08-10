d7adb7c06ba9f7570d4c352fa251c91b0555b3e1
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-08-11 01:42:11 +0530

    garnet: parts: Override HDR types for dolby vision
    Stock display HAL doesn't enable dolby vision support,
    instead miui uses a custom surfaceflinger lib for the
    same. Since we won't be using that, simply override
    supported HDR types at boot.
    
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -10,7 +10,10 @@ package org.lineageos.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.IBinder;
 import android.util.Log;
+import android.view.Display.HdrCapabilities;
+import android.view.SurfaceControl;
 
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
@@ -40,5 +43,11 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
         // Refresh Rate
         RefreshUtils.initialize(context);
+
+        // Override HDR types
+        final IBinder displayToken = SurfaceControl.getInternalDisplayToken();
+        SurfaceControl.overrideHdrTypes(displayToken, new int[]{
+                HdrCapabilities.HDR_TYPE_DOLBY_VISION, HdrCapabilities.HDR_TYPE_HDR10,
+                HdrCapabilities.HDR_TYPE_HLG, HdrCapabilities.HDR_TYPE_HDR10_PLUS});
     }
 }
