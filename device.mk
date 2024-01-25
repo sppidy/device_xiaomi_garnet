20b552862d29255b961fd7022cfc6a71af30a95b
AdarshGrewal <adarshgrewal@gmail.com>
2024-01-25 23:55:19 +0530

    garnet: Switch to common xiaomi vibrator service
    Signed-off-by: Coptan99 <omarcoptan9@gmail.com>

--- device.mk
+++ device.mk
@@ -519,10 +519,10 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    vendor.qti.hardware.vibrator.service.xiaomi
 
 PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    hardware/xiaomi/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # WiFi
 PRODUCT_PACKAGES += \
