3ae093db4aacbf3f4656c9ad75c2aeb33e62fa0d
Bruno Martins <bgcngm@gmail.com>
2024-06-23 12:29:15 +0100

    garnet: Move to new RFS install_symlink targets
    Change-Id: I9560fd0da00958d9349a0881b5a5f64a2c02e495
    Signed-off-by: Coptan99 <omarcoptan9@gmail.com>
    Signed-off-by: sppidy <sppidytg@gmail.com>

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Enable virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
