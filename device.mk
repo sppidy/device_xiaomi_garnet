de5c2035b6111efaa8adcf0b9f82f009c225c428
Ramshouriesh <sppidytg@gmail.com>
2024-06-29 15:27:20 +0000

    garnet : Added some Device Flags

--- device.mk
+++ device.mk
@@ -30,6 +30,10 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 
+# Device Config
+PRODUCT_BOARD_PLATFORM := parrot
+PRODUCT_USES_QCOM_HARDWARE := true
+
 # API
 BOARD_API_LEVEL := 31
 BOARD_SHIPPING_API_LEVEL := 31
@@ -95,7 +99,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths_overlay_static.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_parrot/mixer_paths_overlay_static.xml \
     $(LOCAL_PATH)/configs/audio/mixer_paths_parrot_qrd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_parrot/mixer_paths_parrot_qrd.xml \
     $(LOCAL_PATH)/configs/audio/resourcemanager_parrot_qrd.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_parrot/resourcemanager_parrot_qrd.xml
-
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
